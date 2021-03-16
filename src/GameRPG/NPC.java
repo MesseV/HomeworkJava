5c9eee560846a2e53cca0d7cbd4016aa0950ce76
MesseV <vlad.mesesan1@gmail.com>
2021-03-16 14:08:31 +0200

    RPG random game
    made the whole thing

---
+++ src/GameRPG/NPC.java
@@ -0,0 +1,8 @@
+package GameRPG;
+
+public class NPC extends Character{
+
+    public NPC(int hp, int attackPower, String name) {
+        super(hp, attackPower, name);
+    }
+}
---
+++ src/GameRPG/PlayerCharacter.java
@@ -0,0 +1,8 @@
+package GameRPG;
+
+public class PlayerCharacter extends Character{
+
+    public PlayerCharacter(int hp, int attackPower, String name) {
+        super(hp, attackPower, name);
+    }
+}
