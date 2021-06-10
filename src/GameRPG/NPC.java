5beda8889d3075486045a9ae7d91773678d96966
Vlad Mesesan <vlad.mesesan1@gmail.com>
2021-06-10 08:09:36 +0300

    directory changes

--- src/GameRPG/NPC.java
+++
@@ -1,8 +0,0 @@
-package GameRPG;
-
-public class NPC extends Character{
-
-    public NPC(int hp, int attackPower, String name) {
-        super(hp, attackPower, name);
-    }
-}
--- src/GameRPG/PlayerCharacter.java
+++
@@ -1,8 +0,0 @@
-package GameRPG;
-
-public class PlayerCharacter extends Character{
-
-    public PlayerCharacter(int hp, int attackPower, String name) {
-        super(hp, attackPower, name);
-    }
-}
