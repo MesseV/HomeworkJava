0996aae49be575fdcbd9de048652bf5197413175
Vlad Mesesan <vlad.mesesan1@gmail.com>
2021-06-10 11:53:45 +0300

    tweaks

--- Text-based RPG/src/GameRPG/Main.java
+++ Text-based RPG/src/GameRPG/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        PlayerCharacter player = new PlayerCharacter(40,
+        PlayerCharacter player = new PlayerCharacter(30,
                 15,
                 "Hero",
                 0,
@@ -32,32 +32,31 @@ public class Main {
     public static void battlePhase(PlayerCharacter player, NPC enemy) {
         Scanner reader = new Scanner(System.in);
         while (player.getHp() > 0 && enemy.getHp() > 0) {
-            System.out.println("1. Aggressive stance(+8 att/-3 armour)  -  " +
-                    "2. Defensive stance(-8 att/+3 armour).  -  " +
-                    "3.Balanced stance(-2 att/-0 armour).");
+            System.out.println("1. Aggressive stance(+8 att/-4 res)  -  " +
+                    "2. Defensive stance(-10 att/+3 res).  -  " +
+                    "3.Balanced stance(-2 att).");
             int playerChoice = reader.nextInt();
             if (player.getHp() > 0) {
                 int playerAttack = (int) (Math.random() * (player.getAttackPower() + 1) - 1 + 1);
                 if (playerChoice == 3) {
-                    System.out.println("You strike with a penalty to attack of 2 and no penalty to armour.");
                     if (playerAttack - 2 >= 0) {
+                        enemy.setHp(enemy.getHp() - (playerAttack - 2));
                         System.out.println("You deal " + (playerAttack - 2) + " damage.");
                     } else {
                         System.out.println("You deal 0 damage.");
                     }
-                    enemy.setHp(enemy.getHp() - (playerAttack - 2));
+
                 } else if (playerChoice == 1) {
-                    System.out.println("You lash out with a bonus to attack of 8 and a penalty to armour of 3.");
                     System.out.println("You deal " + (playerAttack + 8) + " damage.");
                     enemy.setHp(enemy.getHp() - (playerAttack + 8));
                 } else if (playerChoice == 2) {
-                    System.out.println("You defend with a penalty to attack of 8 and a bonus to armour of 3.");
-                    if (playerAttack - 8 >= 0) {
-                        System.out.println("You deal " + (playerAttack - 8) + " damage.");
+                    if (playerAttack - 10 >= 0) {
+                        enemy.setHp(enemy.getHp() - (playerAttack - 10));
+                        System.out.println("You deal " + (playerAttack - 10) + " damage.");
                     } else {
                         System.out.println("You deal 0 damage.");
                     }
-                    enemy.setHp(enemy.getHp() - (playerAttack - 8));
+
                 }
                 System.out.println("Enemy life points: " + enemy.getHp());
                 System.out.println("----------------------------------------");
@@ -66,18 +65,33 @@ public class Main {
                 int enemyAttack = (int) (Math.random() * (enemy.getAttackPower() + 1) - 1 + 1);
                 if (playerChoice == 3) {
                     player.setHp(player.getHp() - (enemyAttack - player.getArmour()));
-                    System.out.println("The enemy attacks you and deals " + enemyAttack + " damage. " +
-                            "Your armour blocks " + player.getArmour() + " damage.");
+                    if (enemyAttack >= player.getArmour()) {
+                        System.out.println("The enemy attacks you and deals " + enemyAttack + " damage. " +
+                                "Your armour blocks " + player.getArmour() + " damage.");
+                    } else {
+                        System.out.println("The enemy attacks you and deals " + enemyAttack + " damage. " +
+                                "Your armour blocks all of it.");
+                    }
                 } else if (playerChoice == 1) {
-                    player.setHp(player.getHp() - (enemyAttack + 3));
-                    System.out.println("The enemy attacks you and deals " + (enemyAttack + 3) + " damage. " +
-                            "Your armour blocks " + (player.getArmour() - 1) + " damage.");
+                    player.setHp(player.getHp() - (enemyAttack + 4 - player.getArmour()));
+                    if (enemyAttack+4 >= player.getArmour()) {
+                        System.out.println("The enemy attacks you and deals " + (enemyAttack + 4) + " damage. " +
+                                "Your armour blocks " + player.getArmour() + " of it.");
+                    } else {
+                        System.out.println("The enemy attacks you and deals " + (enemyAttack + 4) + " damage. " +
+                                "Your armour blocks all of it.");
+                    }
                 } else if (playerChoice == 2) {
                     if (enemyAttack - (player.getArmour() + 2) > 0) {
                         player.setHp(player.getHp() - (enemyAttack - (player.getArmour() + 3)));
                     }
-                    System.out.println("The enemy attacks you and deals " + enemyAttack + " damage. " +
-                            "Your armour blocks " + (player.getArmour() + 3) + " damage.");
+                    if (enemyAttack - 3 >= player.getArmour()) {
+                        System.out.println("The enemy attacks you and deals " + enemyAttack + " damage. " +
+                                "Your armour blocks " + player.getArmour() + " of it.");
+                    } else {
+                        System.out.println("The enemy attacks you and deals " + enemyAttack + " damage. " +
+                                "Your armour blocks all of it.");
+                    }
                 }
                 System.out.println("Your life points: " + player.getHp());
                 System.out.println("----------------------------------------");
@@ -109,17 +123,17 @@ public class Main {
             Scanner reader = new Scanner(System.in);
             System.out.println("After the battle you assess your situation - Life points: " + player.getHp());
             System.out.println("You now have " + player.getExperience() + " experience points.");
-            if (player.getExperience() >= (player.getLevel() * 5 + 10)) {
+            while (player.getExperience() >= (player.getLevel() * 5 + 10)) {
                 player.setSkillPoints(player.getSkillPoints() + 1);
                 player.setLevel(player.getLevel() + 1);
-                System.out.println("You've leveled up. You are now level " + player.getLevel() + ". Your stats have increased by 2 and you received 1 skill point..");
+                System.out.println("You've leveled up. You are now level " + player.getLevel() + ". Your stats have increased by 1 and you received 1 skill point..");
                 player.setExperience(player.getExperience() - ((player.getLevel() - 1) * 5 + 10));
-                player.setMaxHp(player.getMaxHp() + 2);
-                player.setHp(player.getHp() + 3);
+                player.setMaxHp(player.getMaxHp() + 1);
+                player.setHp(player.getHp() + 1);
                 if (player.getHp() > player.getMaxHp()) {
                     player.setHp(player.getMaxHp());
                 }
-                player.setAttackPower(player.getAttackPower() + 2);
+                player.setAttackPower(player.getAttackPower() + 1);
             }
             System.out.println("What should your next move be?");
             System.out.println("     1. Rest and heal.");
@@ -131,8 +145,9 @@ public class Main {
             reader.nextLine();
             int randomHeal = (int) (Math.random() * 6 - 1 + 1);
             if (afterBattleChoice == 1) {
-                player.setHp(player.getHp() + 3 + randomHeal);
-                System.out.println("You decide to rest and heal " + (randomHeal+3) + " health.");
+                int healAmount = 3 + randomHeal;
+                System.out.println("You decide to rest and heal " + healAmount + " health.");
+                player.setHp(player.getHp() + healAmount);
                 if (player.getHp() > player.getMaxHp()) {
                     player.setHp(player.getMaxHp());
                 }
@@ -167,6 +182,7 @@ public class Main {
 
     public static void traversingWorld(PlayerCharacter player) {
             System.out.println("Level " + player.getLevel()
+                    + " | Exp: " + player.getExperience() + "/" + (player.getLevel() * 5 + 10)
                     + " | Life points: " + player.getHp() + "/" + player.getMaxHp()
                     + " | Attack power: " + player.getAttackPower()
                     + " | Armour: " + player.getArmour());
