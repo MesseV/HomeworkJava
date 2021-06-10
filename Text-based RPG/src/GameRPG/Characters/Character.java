cb3003ad49c2feae55d779d2ddd1f0ca23c804ba
Vlad Mesesan <vlad.mesesan1@gmail.com>
2021-06-10 08:46:01 +0300

    Directory changes

---
+++ Text-based RPG/src/GameRPG/Characters/Character.java
@@ -0,0 +1,68 @@
+package GameRPG.Characters;
+
+public class Character {
+
+    private String name;
+    private int hp;
+    private int attackPower;
+    private int experience;
+    private int level;
+    private int armour;
+
+    public Character(int hp, int attackPower, String name, int experience, int level, int armour) {
+        this.hp = hp;
+        this.attackPower = attackPower;
+        this.name = name;
+        this.experience = experience;
+        this.level = level;
+        this.armour = armour;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getArmour() {
+        return armour;
+    }
+
+    public void setArmour(int armour) {
+        this.armour = armour;
+    }
+}
---
+++ Text-based RPG/src/GameRPG/Characters/NPC.java
@@ -0,0 +1,8 @@
+package GameRPG.Characters;
+
+public class NPC extends Character{
+
+    public NPC(int hp, int attackPower, String name, int experience, int level, int armour) {
+        super(hp, attackPower, name, experience, level, armour);
+    }
+}
