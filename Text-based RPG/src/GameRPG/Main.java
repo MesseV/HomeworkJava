a6334349b6877b56b70935d59d5efa6dc1d11021
Vlad Mesesan <vlad.mesesan1@gmail.com>
2021-06-17 15:40:32 +0300

    cleaning up

--- Text-based RPG/src/GameRPG/Main.java
+++ Text-based RPG/src/GameRPG/Main.java
@@ -27,7 +27,6 @@ public class Main {
                 + " | Armour: " + player.getArmour());
 
         System.out.println("----------------------------------------");
-
         System.out.println();
 
         while (true) {
