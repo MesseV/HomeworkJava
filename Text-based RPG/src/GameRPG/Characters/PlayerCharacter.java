5f91a38203d0fa4a0c144865a71b7e252c48b540
Vlad Mesesan <vlad.mesesan1@gmail.com>
2021-06-18 08:42:21 +0300

    moved project

--- Text-based RPG/src/GameRPG/Characters/PlayerCharacter.java
+++
@@ -1,8 +0,0 @@
-package GameRPG.Characters;
-
-public class PlayerCharacter extends Character{
-
-    public PlayerCharacter(int hp, int attackPower, String name, int experience, int level, int armour, int maxHp, int skillPoints) {
-        super(hp, attackPower, name, experience, level, armour, maxHp, skillPoints);
-    }
-}
